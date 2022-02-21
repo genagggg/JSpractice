069f75fe81efd982e75146ae1e4e7b7063dd4765
Gena <mr.genag@mail.ru>
2022-02-21 19:52:35 +0500

    Lesson number _1 Object for_in

---
+++ lesson-1/README.md
@@ -0,0 +1,3 @@
+### Обьекты в JavaScript #
+## Синтаксис, методы методы обьектов #
+https://www.youtube.com/watch?v=ACXQO1YZOXI&list=PLM6XATa8CAG4F9nAIYNS5oAiPotxwLFIr&index=44 Ссылка на видео, автор Жека Андриканич, время урока 31:33.  
---
+++ lesson-1/script.js
@@ -0,0 +1,133 @@
+/*let userInfo = new Object();//синтаксис "конструктор обьекта"
+let userInfo = {};//синтаксис "литерала обьекта"
+
+let userInfo = {
+    name: "Выся",
+    age: 30,  //Висячая запятая
+    "likes javascript": true
+}
+console.log(userInfo);
+//console.log(userInfo.name)
+console.log(userInfo["likes javascript"]);
+
+let userInfo ={
+    name: "Вася",
+    age: 30,
+    "like javascript": true
+}
+
+console.log(userInfo);
+
+delete userInfo.age;
+
+console.log(userInfo);
+delete userInfo["like javascript"];
+console.log(userInfo);
+
+
+
+
+//Копирование обьекта
+
+//Прикопировании обьекта в другую пременную
+//сам обьект не дублируется, а копируется только ссылкана него
+
+let userInfo={
+    name: "Вася",
+    age: 30,
+}
+
+console.log(userInfo);
+
+let user =userInfo;
+console.log(user);
+user.age=18;
+console.log(userInfo);
+
+
+//Дублирование обьектов (Object.assign)
+
+let userInfo = {
+    name: "Вася",
+    age: 30,
+}
+
+let user = Object.assign({}, userInfo);
+
+user.age = 18;
+
+console.log(userInfo);//{name: "Вася", age: 30}
+console.log(user);//{name:"Вася", age:18}
+
+
+// Еще про Object.assign
+
+let userInfo ={
+    name: "Вася",
+    age: 30,
+}
+
+Object.assign(userInfo, {['like javascript']: true, city: "Uzhhorod"})
+console.log(userInfo);
+
+//проверка существования свойства
+
+let userInfo ={
+    name: "Вася",
+    age: 30,
+}
+
+if(userInfo.age){
+    console.log(userInfo.age);
+}
+
+//Проверка существования свойства
+
+// Опциональная цепочка
+let userInfo ={
+    name: "Вася",
+    age: 30,
+    //address: {
+        //city: "Uzhhorod",
+        //street: "Freedom",
+    }
+}
+
+console.log(userInfo?.addres?.street);
+
+//Проверка существования свойства
+
+// Оператор "in"
+let userInfo ={
+    name: "Вася",
+    age: 30,
+    address: {
+        city: "Uzhhorod",
+        street: "Freedom",
+    }
+}
+
+if ("name" in userInfo) {
+    console.log(userInfo.name);
+}
+
+//Цикл "for_in"
+*/
+
+let userInfo = {
+    name: "Gena",
+    age: 31,
+    address: {
+        city: "V_Salda",
+        street: "Evstigneeva"
+    }
+}
+for (let key in userInfo) {
+    console.log(key)
+    console.log(userInfo[key])
+}
+
+for (let key in userInfo.address) {
+    console.log(key);
+    console.log(userInfo.address[key]);
+}
