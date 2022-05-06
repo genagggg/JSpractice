b083f236983fcc1be1dceee85bf9cf6949e941b0
Gena <mr.genag@mail.ru>
2022-05-06 20:54:05 +0500

    06.05.22

--- 06.05.2022/script.js
+++ 06.05.2022/script.js
@@ -145,3 +145,78 @@
 // setTimeout(timeout2sec, 2000)
 
 // console.log('End')
+
+// ================================================
+// Promise
+// console.log('Request data...')
+
+// setTimeout(() => {
+//     console.log('Preparing data...')
+//     const backendData = {
+//         server: 'aws',
+//         port: 2000,
+//         status: 'working'
+//     }
+//     setTimeout(() => {
+//         backendData.modified = true
+//         console.log('Data received', backendData)
+//     }, 2000)
+// }, 2000)
+
+// const p = new Promise(function (resolve, reject) {
+//     setTimeout(() => {
+//         console.log('Preparing data ...')
+//         const backendData = {
+//             server: 'aws',
+//             port: 2000,
+//             status: 'working'
+//         }
+//         resolve()
+//     }, 2000)
+// })
+
+// p.then(() => {
+//     console.log('Promise resolve')
+// })
+
+// console.log('Request Data ....')
+
+// setTimeout(function () {
+//     console.log('Preparing Data ...')
+//     const backendData = {
+//         server: 'aws',
+//         port: 2000,
+//         status: 'working'
+//     }
+//     setTimeout(function () {
+//         backendData.modified = true
+//         console.log('Data received', backendData)
+//     }, 2000)
+// }, 2000)
+
+// const p = new Promise(function (resolve, reject) {
+//     setTimeout(() => {
+//         console.log('Preparing data...')
+//         const backendData = {
+//             server: 'aws',
+//             port: 2000,
+//             status: 'working'
+//         }
+//         resolve(backendData)
+//     }, 2000)
+// })
+
+// p.then(data => {
+//     const p2 = new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             data.modifide = true
+//             resolve(data)
+//         }, 2000)
+//     })
+//     p2.then(
+//         clientData => {
+//             console.log('Data received', clientData)
+//         }
+//     )
+// })
+
